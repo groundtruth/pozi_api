36542b05ab6061f69a1430e4d0614b5426214c5a
Chris Berkhout <chrisberkhout@gmail.com>
2013-05-02 14:43:22 +1000

    Fail hard if misused and let Sinatra handle producing an appropriate HTTP response.

--- lib/restful_geof/app.rb
+++ lib/restful_geof/app.rb
@@ -3,6 +3,7 @@ require "restful_geof/routes"
 
 module RestfulGeof
   class App < Sinatra::Base
+    configure { set :raise_errors, false } # fail hard and let Sintra handle it, in all environments
     post(//) { Routes.route(request) }
     get(//) { Routes.route(request) }
     put(//) { Routes.route(request) }
