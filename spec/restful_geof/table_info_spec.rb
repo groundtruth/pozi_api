dcf015acdc1f44532b9b1abf12ea5980e3df2bc1
Chris Berkhout <chrisberkhout@gmail.com>
2013-05-20 20:07:11 +1000

    Added specs for newly extracted Query class.

--- spec/restful_geof/table_info_spec.rb
+++ spec/restful_geof/table_info_spec.rb
@@ -8,6 +8,7 @@ module RestfulGeof
       TableInfo.new([
         { :column_name => "id", :udt_name => "integer" },
         { :column_name => "name", :udt_name => "varchar" },
+        { :column_name => "bignum", :udt_name => "int8" },
         { :column_name => "the_geom", :udt_name => "geometry" },
         { :column_name => "search_text_one", :udt_name => "tsvector" },
         { :column_name => "search_text_two", :udt_name => "tsvector" }
@@ -28,13 +29,17 @@ module RestfulGeof
 
     describe "#normal_columns" do
       it "should identify normal columns for shoing in properties part of GeoJSON" do
-        subject.normal_columns.should == ["id", "name"]
+        subject.normal_columns.should == ["id", "name", "bignum"]
       end
     end
 
     describe "#integer_col?" do
-      it "should work" do
-        pending
+      it "should return true if the column is any integer type" do
+        subject.integer_col?("id").should be_true
+        subject.integer_col?("bignum").should be_true
+      end
+      it "should return false if the column is not an integer type" do
+        subject.integer_col?("name").should be_false
       end
     end
 
