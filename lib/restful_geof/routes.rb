9711d705b22a2e958328ec974eb0d86ce33a32af
Chris Berkhout <chrisberkhout@gmail.com>
2013-08-11 15:13:28 +0200

    Finished basic Routes refactor.

--- lib/restful_geof/routes.rb
+++ lib/restful_geof/routes.rb
@@ -11,8 +11,14 @@ module RestfulGeof
       @body = request.body.read
     end
 
-    def match_find_path
-      if @path_info.match(%r{
+    def params
+      query_request_params || crud_request_params || { :action => :unknown } 
+    end
+
+    private
+
+    def query_request_params
+      if @request_method == "GET" && @path_info.match(%r{
         ^
         /(?<database>[^/]+)
         /(?<table>[^/]+)
@@ -20,11 +26,31 @@ module RestfulGeof
         (/limit/(?<limit>\d+))?
         $
       }x)
-        @matched_database = URI.unescape $~[:database].to_s
-        @matched_table = URI.unescape $~[:table].to_s
-        @matched_limit = URI.unescape $~[:limit].to_s
-        @matched_conditions_raw = $~[:conditions_string].to_s.scan(%r{/(?<part1>[^/]+)/(?<part2>[^/]+)/(?<part3>[^/]+)}x)
-        return true
+        main_match = $~
+        condition_options = { :is => {}, :in => {}, :matches => {}, :contains => {}, :closest => {} }
+
+        condition_options[:limit] = URI.unescape(main_match[:limit].to_s).to_i unless URI.unescape(main_match[:limit].to_s).empty?
+
+        main_match[:conditions_string].to_s.scan(%r{/(?<part1>[^/]+)/(?<part2>[^/]+)/(?<part3>[^/]+)}x).each do |condition_raw|
+          part1, part2, part3 = condition_raw.map { |str| URI.unescape str }
+          if part2.is_in?(%w{is matches contains})
+            condition_options[part2.to_sym][part1] = part3
+          elsif part2 == "in"
+            condition_options[:in][part1] = condition_raw.last.split(",").map { |str| URI.unescape str }
+          elsif part1 == "closest"
+            condition_options[:closest][:lon] = part2
+            condition_options[:closest][:lat] = part3
+          else
+            return { :action => :unknown }
+          end
+        end
+
+        {
+          :action => :find,
+          :database => URI.unescape(main_match[:database].to_s),
+          :table => URI.unescape(main_match[:table].to_s),
+          :conditions => condition_options
+        }
       end
     end
 
@@ -37,7 +63,7 @@ module RestfulGeof
         $
       }x)
         valid_params(
-          trim_params({
+          without_empties({
             :action => crud_action,
             :database => URI.unescape($~[:database].to_s),
             :table => URI.unescape($~[:table].to_s),
@@ -57,52 +83,18 @@ module RestfulGeof
       }[@request_method]
     end
 
-    def trim_params(params)
+    def without_empties(params)
       Hash[params.map { |k,v| [k, v] unless v.empty? }.compact]
     end
 
     def valid_params(params)
-      params.keys == {
+      keys_required_by_action = {
         :read =>   [:action, :database, :table, :id],
         :create => [:action, :database, :table, :body_json],
         :delete => [:action, :database, :table, :id],
         :update => [:action, :database, :table, :id, :body_json]
-      }[crud_action] && params
-    end
-
-    def params
-
-      if @request_method == "GET" && match_find_path
-
-        condition_options = { :is => {}, :in => {}, :matches => {}, :contains => {}, :closest => {} }
-        condition_options[:limit] = @matched_limit.to_i unless @matched_limit.empty?
-        @matched_conditions_raw.each do |condition_raw|
-          part1, part2, part3 = condition_raw.map { |str| URI.unescape str }
-          if part2.is_in?(%w{is matches contains})
-            condition_options[part2.to_sym][part1] = part3
-          elsif part2 == "in"
-            condition_options[:in][part1] = condition_raw.last.split(",").map { |str| URI.unescape str }
-          elsif part1 == "closest"
-            condition_options[:closest][:lon] = part2
-            condition_options[:closest][:lat] = part3
-          else
-            return { :action => :unknown }
-          end
-        end
-
-        return {
-          :action => :find,
-          :database => @matched_database,
-          :table => @matched_table,
-          :conditions => condition_options
-        }
-
-      elsif crud_request_params
-        crud_request_params
-      else
-        { :action => :unknown }
-      end
-
+      }
+      params if params.keys == keys_required_by_action[crud_action]
     end
 
   end
