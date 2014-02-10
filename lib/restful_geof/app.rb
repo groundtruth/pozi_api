880020f494ce499e47f756cf13fa928d5776b96e
Chris Berkhout <chrisberkhout@gmail.com>
2014-02-10 15:41:42 +1100

    Cors headers need to go on every request.

--- lib/restful_geof/app.rb
+++ lib/restful_geof/app.rb
@@ -7,17 +7,20 @@ module RestfulGeof
       set :raise_errors, true
       set :show_exceptions, false
     end
-    post(//) { Controller.handle(request) }
-    get(//) { Controller.handle(request) }
-    put(//) { Controller.handle(request) }
-    delete(//) { Controller.handle(request) }
 
-    options(//) {
-      response.headers["Access-Control-Allow-Origin"] = "*"
-      response.headers["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept"
-      response.headers["Access-Control-Allow-Methods"] = "POST"
-      204
-    } if ENV["RESTFUL_GEOF_OPEN_CORS"] == 'true'
+    def cors_headers
+      if ENV["RESTFUL_GEOF_OPEN_CORS"] == 'true'
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        response.headers["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept"
+        response.headers["Access-Control-Allow-Methods"] = "POST"
+      end
+    end
+
+    post(//) { cors_headers; Controller.handle(request) }
+    get(//) { cors_headers; Controller.handle(request) }
+    put(//) { cors_headers; Controller.handle(request) }
+    delete(//) { cors_headers; Controller.handle(request) }
+    options(//) { cors_headers; 204 }
 
     run! if app_file == $0 # start the server if this file is executed directly
   end
